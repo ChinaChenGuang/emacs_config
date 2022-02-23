fb9ca8f988fad548a7b581580a1a5318ab23b67f
ChinaChenGuang <1443645743@qq.com>
2022-02-23 15:07:30 +0800

    add custom.el

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(lsp-clients-svlangserver-formatCommand "/tools/verible-verilog-format")
+ '(lsp-clients-svlangserver-launchConfiguration "/tools/verilator -sv --lint-only -Wall")
+ '(lsp-vhdl-server (quote hdl-checker))
+ '(package-selected-packages
+   (quote
+    (neotree company-ctags company lsp-mode auto-company gruvbox-theme restart-emacs use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
